dad40904b7fb08f5f14a2c7fc8b2669f4a5c9921
erica223020 <erica223020@gmail.com>
2022-06-29 19:43:16 +0800

    22.6.29 新增 README.md

---
+++ README.md
@@ -0,0 +1,21 @@
+# 響應式網頁設計-三角香氛
+### 以香氛作為主題，製作一個形象網站
+&nbsp;
+
+
+#  歷史版本
+* 1.0
+    * 新增:建立`index.html`
+* 1.1
+    * 新增:建立`README.md`
+
+&nbsp;
+# 軟體使用
+* 設計圖繪製 : AdobeXD
+* 程式設計 ： VSCode
+* 圖片編輯 ： Adobe Photoshop
+
+&nbsp;
+# 參考資料
+* HANSCHIU 品牌官方網站
+* medskin 品牌官方網站
---
+++ header.js
@@ -0,0 +1,15 @@
+header = '<header>\
+<nav>\
+    <h1><a href="#" class="logo"></a></h1>\
+    <ul class="menu">\
+        <li><a href="#">首頁</a></li>\
+        <li><a href="#">登入</a></li>\
+        <li><a href="#">線上預購</a></li>\
+    </ul>\
+</nav>\
+<section class="banner">\
+    <img src="https://upload.cc/i1/2022/06/28/iwyJdK.png" alt="banner">\
+    <h2 class="banner_slogan">aaa</br>aaa</h2>\
+</section>\
+</header>';
+document.write(header);
