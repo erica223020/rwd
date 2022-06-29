094025d73f2cf52bbf3bec42b7894d17f91c3fa0
erica223020 <erica223020@gmail.com>
2022-06-29 19:55:52 +0800

    22.6.29 新增footer

---
+++ footer.js
@@ -0,0 +1,15 @@
+footer = '<footer>\
+    <ul class="contact">\
+        <li><h2><a href="#" class="logo"></a></h2></li>\
+        <li><a class="icon_fb" href="#"><img src=""></a></li>\
+        <li><a class="icon_google" href="#"><img src=""></a></li>\
+        <li><a class="icon_line" href="#"><img src=""></a></li>\
+    </ul>\
+    <ul class="information">\
+        <li class="address"><img src="#" alt="">地址:台北市松山區三角路\
+        </li>\
+        <li class="tel"><img src="#" alt="">電話:02-12345678</li>\
+        <li class="mail"><img src="#" alt="">E-mail:triangle@gmail.com</li>\
+    </ul>\
+</footer>';
+document.write(footer);
