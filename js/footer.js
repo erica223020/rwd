c744de5b5bb50952a4dd7e860021e54eb4ab22ec
erica223020 <erica223020@gmail.com>
2022-07-05 20:59:57 +0800

    22.7.5
    新增SCSS架構，完成base.scss

---
+++ js/footer.js
@@ -0,0 +1,15 @@
+footer = '<footer>\
+<ul class="contact">\
+    <li><h2 class="logo_2">三角香氛</h2></li>\
+    <li><i class="fa-brands fa-facebook-f"></i></li>\
+    <li><i class="fa-brands fa-google-plus-g"></i></li>\
+    <li><i class="fa-brands fa-line"></i></li>\
+</ul>\
+<ul class="information">\
+    <li class="address"><i class="fa-solid fa-location-dot"></i>地址:台北市松山區三角路\
+    </li>\
+    <li class="tel"><i class="fa-solid fa-phone"></i>電話:02-12345678</li>\
+    <li class="mail"><i class="fa-solid fa-envelope"></i>E-mail:triangle@gmail.com</li>\
+</ul>\
+</footer>';
+document.write(footer);
---
+++ js/header.js
@@ -0,0 +1,14 @@
+header = '<header>\
+<nav class="nav">\
+    <h1 class="logo"><a href="#">三角香氛</a></h1>\
+    <ul class="menu">\
+        <li><a href="index.html">首頁</a></li>\
+        <li><a href="login.html">登入</a></li>\
+        <li><a href="pre-order.html">線上預購</a></li>\
+    </ul>\
+</nav>\
+<section class="banner">\
+    <h2 class="banner_slogan">無法忘懷的香氣</br>享受獨特的個性</h2>\
+</section>\
+</header>';
+document.write(header);
