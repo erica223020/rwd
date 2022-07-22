e75b1cecca16d4995dfdcaa8f06e2fca81a3f397
erica223020 <erica223020@gmail.com>
2022-07-22 20:54:28 +0800

    22.7.22
    新增index.scss

--- js/footer.js
+++ js/footer.js
@@ -1,11 +1,14 @@
 footer = '<footer>\
-<ul class="contact">\
-    <li><h2 class="logo_2">三角香氛</h2></li>\
+<div class="footer_link">\
+    <h2 class="logo_2">三角香氛</h2>\
+    <ul class="link">\
     <li><i class="fa-brands fa-facebook-f"></i></li>\
-    <li><i class="fa-brands fa-google-plus-g"></i></li>\
+    <li><i class="fa-brands fa-google"></i></li>\
     <li><i class="fa-brands fa-line"></i></li>\
 </ul>\
-<ul class="information">\
+</div>\
+<i class="fa-solid fa-grip-lines-vertical"></i>\
+<ul class="footer_information">\
     <li class="address"><i class="fa-solid fa-location-dot"></i>地址:台北市松山區三角路\
     </li>\
     <li class="tel"><i class="fa-solid fa-phone"></i>電話:02-12345678</li>\
