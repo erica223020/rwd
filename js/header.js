ad4b3e52cffea5ec21a57206808acd57a800046d
erica223020 <erica223020@gmail.com>
2022-07-26 20:49:29 +0800

    新增 maxin

--- js/header.js
+++ js/header.js
@@ -6,6 +6,7 @@ header = '<header>\
         <li><a href="login.html">登入</a></li>\
         <li><a href="pre-order.html">線上預購</a></li>\
     </ul>\
+    <a href="#" class="show"><i class="fa-solid fa-bars"></i></a>\
 </nav>\
 <section class="banner">\
     <h2 class="banner_slogan slogan1">無法忘懷的香氣\
